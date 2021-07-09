f10cba61f868cea95fcff8ec0291fc895d25c4b7
Kira <kiryusha-ok@yandex.ru>
2021-07-09 12:28:10 +0300

    Меню JS

--- javascript/book.js
+++ javascript/book.js
@@ -2,4 +2,15 @@ $(document).ready(function() {
 	let greeting;
 	$('#pre').text( greeting );
 	$('#pre').fadeIn(5000);
+	
+	$('#pulldown').hide();
+	$('#nav').hover(
+		function(){
+		$('#pulldown').fadeToggle(300);
+	});
 });
+
+
+
+ 
+ 
