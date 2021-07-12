e1d13db03e49c94b64c6181e6430e8f2badf4e15
Kira <kiryusha-ok@yandex.ru>
2021-07-13 02:04:26 +0300

    ending:gallery,media

--- javascript/galery.js
+++ javascript/galery.js
@@ -1,7 +1,17 @@
 $(document).ready(function() {
 	$('.small img').click(function() {
 		let url = this.src.replace('_small.jpg', '_medium.jpg');
-		if($('#medium img').src != url)
-			$('#medium img')[0].outerHTML = '<img src="' + url + '">';
+		if($('#medium img').prop('src') != url)
+			$('#medium img').prop('src', url);
+	});
+	$('#medium img').click(function() {
+		let e = document.getElementById('big');
+		let url = this.src.replace('_medium.jpg', '_big.jpg');
+		e.style.display = 'block';
+		if($('#big img').prop('src') != url)
+			$('#big img').prop('src', url);
+	});
+	$('#big').click(function() {
+		document.getElementById('big').style.display = 'none';
 	});
 });
--- javascript/slider.js
+++ javascript/slider.js
@@ -1,8 +1,13 @@
 $(function(){
-    sliderRun(); // запускаем нашу программу, когда готов DOM
+    $w = $('.slide').width();
+	$(window).on('resize', function() {
+		$w = $('.slide').width();
+	});
+	sliderRun(); // запускаем нашу программу, когда готов DOM
 });
+let $w;
 function sliderRun() {
-    const w = $('.slide').width(); // ширина слайда - самая важная константа, ее везде используем
+    //const w = $('.slide').width(); // ширина слайда - самая важная константа, ее везде используем
     const t = 3000; // время в миллисекундах, вынесено в константу для удобства. для таймаута я взял удвоенный интервал.
     let current = 0; // указатели на текущий слайд и его соседей слева и справа
     let left = -1; // -1 соответствует length-1, то есть последнему слайду из набора
@@ -11,15 +16,15 @@ function sliderRun() {
     let flag2 = false;
     let flag3 = false;
     $('.slide').eq(current).addClass('active').css('left', 0);
-    $('.slide').eq(left).css('left', -w); // располагаем левый и правый слайды по бокам от основного. поскольку overflow: hidden - их не видно. используем одно и то же свойство left для всех слайдов.
-    $('.slide').eq(right).css('left', w);
+    $('.slide').eq(left).css('left', -$w); // располагаем левый и правый слайды по бокам от основного. поскольку overflow: hidden - их не видно. используем одно и то же свойство left для всех слайдов.
+    $('.slide').eq(right).css('left', $w);
     function moveLeft() {
         if (flag1 || flag2 || flag3) return; // проверяем флаги, если хоть один не убран, отказываемся действовать. это не позволит одновременно выполняться вызовам от постоянной прокрутки и кнопок или от двух кнопок сразу.
         flag1 = true; // поднимаем все флаги, чтобы не допустить нового вызова, пока не отработал текущий
         flag2 = true;
         flag3 = true;
         left = current; // текущий слайд становится левым и уезжает налево
-        $('.slide').eq(left).animate({left: -w}, t, function(){
+        $('.slide').eq(left).animate({left: -$w}, t, function(){
             $(this).removeClass('active');
             flag1 = false; // когда анимация отработала, один флаг убирается
         });
@@ -31,7 +36,7 @@ function sliderRun() {
         if (right > $('.slide').length - 2) {
             right -= $('.slide').length; // если значение указателя больше индекса предпоследнего слайда, уменьшаем на его количество слайдов: последний слайд получается по индексу -1.
         }
-        $('.slide').eq(right).css('left', w); // устанавливаем правый слайд в готовности справа. поскольку он не должен быть виден - без анимации.
+        $('.slide').eq(right).css('left', $w); // устанавливаем правый слайд в готовности справа. поскольку он не должен быть виден - без анимации.
         flag3 = false; // убираем третий флаг
     }
     function moveRight() { // все аналогично предыдущей функции, только в обратную сторону
@@ -40,7 +45,7 @@ function sliderRun() {
         flag2 = true;
         flag3 = true;
         right = current;
-        $('.slide').eq(right).animate({left: w}, t, function(){
+        $('.slide').eq(right).animate({left: $w}, t, function(){
             $(this).removeClass('active');
             flag1 = false;
         });
@@ -52,7 +57,7 @@ function sliderRun() {
         if (left < -1) {
             left += $('.slide').length;
         }
-        $('.slide').eq(left).css('left', -w);
+        $('.slide').eq(left).css('left', -$w);
         flag3 = false;
     }
     let tm = setTimeout(everScroll, t * 2); // назначаем вызов вспомогательной функции с задержкой
