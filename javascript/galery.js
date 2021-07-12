05f4bd15e802d6ab765f5bce82f35defdd80bd55
Kira <kiryusha-ok@yandex.ru>
2021-07-12 12:41:54 +0300

    ending gallery

--- javascript/galery.js
+++ javascript/galery.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	$('.small img').click(function() {
-		let url = this.src.split('_small.jpg').join('_medium.jpg');
+		let url = this.src.replace('_small.jpg', '_medium.jpg');
 		if($('#medium img').src != url)
 			$('#medium img')[0].outerHTML = '<img src="' + url + '">';
 	});
